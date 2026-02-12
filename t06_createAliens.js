b2c4165096f9885f1e5eb6ada692f6e4c4074407
Jackson Li <23209zl@hvhs.school.nz>
2026-02-13 09:52:39 +1300

    Spell 1 Friday

--- t06_createAliens.js
+++ t06_createAliens.js
@@ -9,50 +9,52 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
-cnv = new Canvas(900, 900);
+  cnv = new Canvas(900, 900);
 
-ball_1 = new Sprite(width/2, height/2, 50, 'd');
+  ball_1 = new Sprite(width/2, height/2, 50, 'd');
 
-ball_1.color = 'cyan';
+  ball_1.color = 'cyan';
 
-ball_1.vel.x = 2;
+  ball_1.vel.x = 2;
 
-ball_1.bounciness = 1;
+  ball_1.bounciness = 1;
 
-ball_1.friction = 0;
+  ball_1.friction = 0;
 
-ball_1.drag = 0;
+  ball_1.drag = 0;
 
-wallLH  = new Sprite(0, height/2, 8, height, 'k');
+  wallLH  = new Sprite(0, height/2, 8, height, 'k');
 
-wallLH.color = 'black';
+  wallLH.color = 'black';
 
-wallRH  = new Sprite(900, 450, 8, 1000, 'k');
+  wallRH  = new Sprite(900, 450, 8, 1000, 'k');
 
-wallRH.color = 'black';
+  wallRH.color = 'black';
 
-wallTop = new Sprite(500, 0, 1000, 8, 'k');
+  wallTop = new Sprite(500, 0, 1000, 8, 'k');
 
-wallTop.color = 'black'
+  wallTop.color = 'black';
 
-wallBot = new Sprite(0, 900, 1800, 8, 'k');
+  wallBot = new Sprite(0, 900, 1800, 8, 'k');
 
-wallBot.color = 'black'
+  wallBot.color = 'black';
+  
+  randNum = random(6, 8);
 
-for (i = 0; i < 100; i++) {
 
-  alien = new Sprite();
+  for (i = 0; i < 100; i++) {
 
-  alien.vel.x = 3;
+    alien = new Sprite();
 
-  alien.vel.y = 4;
+    alien.vel.x = 3;
 
-  alien.bounciness = 1;
+    alien.vel.y = 4;
 
-  alien.friction = 0;
+    alien.bounciness = 1;
 
-}
+    alien.friction = 0;
 
+  }
 }
 	
 
